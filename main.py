a07f08e5f038b790507e841fc412f297b30dad13
DEDZTBH <peiqial@outlook.com>
2020-07-07 14:52:21 +0800

    reorganize code and adjust params

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 import os
 import pickle
 import torch.nn.functional as F
+import cv2
+
+from filters import lowpass, highpass
 
 data_dir = './data/'
 C = 2048
@@ -20,56 +23,39 @@ else:
         if dirname.startswith('LuoJia1'):
             im = Image.open('{}{}/{}_gec.tif'.format(data_dir, dirname, dirname))
             np_img = np.asarray(im.getdata())
+            # restore normal FP representation, but magnified 1e5 times or it's too dark
             np_img = np_img ** (3 / 2) * 1e-5
             w, h = im.size
             np_img.shape = (h, w)
-            im = Image.fromarray(np_img)
-            im = im.resize((R, C))
-            np_img = np.asarray(im.getdata())
-            w, h = im.size
-            np_img.shape = (h, w)
-            images.append(np_img)
+            images.append(cv2.resize(np_img, (R, C)))
     with open('images.pkl', 'wb') as f:
         pickle.dump(images, f)
 
 
-def bandpass(x, lo, hi):
-    shape = x.shape
-    avg = (lo + hi) / 2
-    diff = hi - avg
-    x = np.where(abs(x - avg) <= diff, x, 0)
-    x.shape = shape
-    return x
+def show_tensor(out_img):
+    Image.fromarray(out_img.squeeze().numpy()).show()
 
 
-def highpass(x, lo):
-    shape = x.shape
-    x = np.where(lo <= x, x, 0)
-    x.shape = shape
-    return x
-
-
-def lowpass(x, hi):
-    shape = x.shape
-    x = np.where(x <= hi, x, 0)
-    x.shape = shape
-    return x
-
-
-img = images[2]
+img = images[1]
 
 Image.fromarray(img).show()
 
-filter = np.genfromtxt('filter.csv', delimiter=',')
-filter.shape = (1, 1, 3, 3)
-filter = torch.from_numpy(filter)
-
+kernel = np.genfromtxt('filter.csv', delimiter=',')
+kernel.shape = (1, 1, 3, 3)
+kernel = torch.from_numpy(kernel)
 img_t = torch.from_numpy(img)
 img_t = img_t.unsqueeze(0).unsqueeze(0)
-out_img = F.conv2d(img_t, filter, padding=1)  # unsharp mask
-out_img = torch.from_numpy(lowpass(out_img.numpy(), 60))
+out_img = F.conv2d(img_t, kernel, padding=1)  # unsharp mask
+# show_tensor(out_img)  # streetlights are sharpened and cloud remains
+
+out_img = torch.from_numpy(lowpass(out_img.numpy(), 70))
+# show_tensor(out_img)  # now only cloud remains
+
 out_img = F.avg_pool2d(out_img, 8)
-out_img = highpass(out_img.squeeze().numpy(), 15) * 10
-out_img = Image.fromarray(out_img)
-out_img = out_img.resize((C, R))
-out_img.show()
+# show_tensor(out_img * 10)  # filtered out streetlights
+
+out_img = highpass(out_img.squeeze().numpy(), 10)  # remove noise
+
+out_img_rgb = np.uint8(np.round(np.dstack((img, cv2.resize(out_img, (R, C)) * 10, np.zeros((R, C))))))
+out_img_rgb = Image.fromarray(out_img_rgb)
+out_img_rgb.show()
