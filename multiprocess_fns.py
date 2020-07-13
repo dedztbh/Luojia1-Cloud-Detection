8d7956594b860178f8e562a9fb67935cb3ae9b4e
DEDZTBH <peiqial@outlook.com>
2020-07-13 16:44:19 +0800

    adding more post processing

---
+++ multiprocess_fns.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import numpy as np
+import cv2
+from globals import data_dir, R, C
+
+
+# from multiprocessing import Pool, get_context
+
+
+def get_img(dirname):
+    im = Image.open('{}{}/{}_gec.tif'.format(data_dir, dirname, dirname))
+    np_img = np.asarray(im.getdata())
+    # restore normal FP representation, but magnified 1e5 times or it's too dark
+    np_img = np_img ** (3 / 2) * 1e-5
+    w, h = im.size
+    np_img.shape = (h, w)
+    return cv2.resize(np_img, (R, C))
+
+
+# From https://stackoverflow.com/questions/42798659/how-to-remove-small-connected-objects-using-opencv/42812226
+def remove_small_obj(img):
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=8)
+    # connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    # the following part is just taking out the background which is also considered a component, but most of the time
+    # we don't want that.
+    sizes = stats[1:, -1]
+    nb_components = nb_components - 1
+
+    # minimum size of particles we want to keep (number of pixels)
+    # here, it's a fixed value, but you can set it as you want, eg the mean of the sizes or whatever
+    min_size = 200
+
+    # your answer image
+    img2 = np.zeros(output.shape)
+    # for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+            img2[output == i + 1] = 1
+
+    return img2
+
+
+def unsharp(image):
+    unsharp_kernel = np.genfromtxt('unsharp.csv', delimiter=',')
+    return cv2.filter2D(image, -1, unsharp_kernel)
+
+
+def pmap(f, things):
+    # with get_context('spawn').Pool() as p:
+    #     return list(p.map(f, things))
+    return list(map(f, things))
