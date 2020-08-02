f8bce808744123472c89c66121f95fd6f94915cd
DEDZTBH <peiqial@outlook.com>
2020-08-02 17:24:46 +0800

    auxiliary file update, update flowchart

--- flowchart_gen.py
+++ flowchart_gen.py
@@ -23,21 +23,21 @@ def show_imgs(imgs, name='flowchart'):
     for i in range(1, columns * rows + 1):
         ax = fig.add_subplot(rows, columns, i)
         ax.title.set_text(imgs[idx][0])
-        plt.imshow(Image.fromarray(imgs[idx][1] * 5).convert('L'))
+        plt.imshow(Image.fromarray(imgs[idx][1]).convert('L'))
         idx += 1
     plt.savefig('{}.png'.format(name), bbox_inches='tight')
 
 
 def main():
     img = get_img('LuoJia1-01_LR201809083317_20180907212040_HDR_0033')
-    imgs = [('Original', img)]
+    imgs = [('Original', img * 5)]
     imgdiffs = [('Original', np.zeros((2000, 2000)))]
     for f in cloud_mask_generate_procedure_binary:
         img2 = deepcopy(img)
         img = f(img)
         title = f.__name__.replace('_', ' ').title()
-        imgs.append((title, img))
-        imgdiffs.append((title, img2 - img))
+        imgs.append((title, img * 5))
+        imgdiffs.append((title, (img2 - img) * 5))
 
     show_imgs(imgs)
     show_imgs(imgdiffs, name='diff')
--- image_tester.py
+++ image_tester.py
@@ -1,35 +1,37 @@
 from PIL import Image
 import numpy as np
 import cv2
+import os
 
-from core import cloud_mask_generate_procedure_binary
+from core import cloud_mask_generate_procedure_binary, pmap, get_img
+from global_const import data_dir
 
 
-def save_img(img, name='out'):
+def save_img(img, name):
     Image.fromarray(img).convert('RGB').save('{}.png'.format(name))
 
 
-def main():
-    im = Image.open('ISS035-E-17200.JPG')
-    np_img = np.asarray(im.convert('L').getdata()).astype(np.float32)
-    w, h = im.size
-    np_img.shape = (h, w)
-    img = cv2.resize(np_img, (2000, 2000))
-    # img = deepcopy(np_img)
+def do_one(name, np_img, w=2000, h=2000):
+    if w != 2000 or h != 2000:
+        img = cv2.resize(np_img, (2000, 2000))
+    else:
+        img = np_img
 
     for f in cloud_mask_generate_procedure_binary:
         img = f(img)
 
-    img = cv2.resize(img, (w, h)).astype(np.uint8)
-    img = img.astype(np.uint8)
+    if w != 2000 or h != 2000:
+        img = cv2.resize(img, (w, h)).astype(np.uint8)
+    else:
+        img = img.astype(np.uint8)
 
-    save_img(img * 70)
+    save_img(img * 70, 'output/{}'.format(name))
 
     out_img_rgb = np.dstack((np.uint8(np.round(np_img)),
                              safe_mult(img, 70),
                              np.zeros((h, w), dtype=np.uint8)))
 
-    save_img(out_img_rgb, name='overlay')
+    save_img(out_img_rgb, 'overlay/{}'.format(name))
 
 
 def safe_mult(x, v):
@@ -39,5 +41,32 @@ def safe_mult(x, v):
     return x * v
 
 
+def wrapper(x):
+    do_one(x[0], x[1])
+
+
 if __name__ == '__main__':
-    main()
+    # name = 'ISS035-E-17200.JPG'
+    # im = Image.open(name)
+    # np_img = np.asarray(im.convert('L').getdata()).astype(np.float32)
+    # w, h = im.size
+    # np_img.shape = (h, w)
+    # do_one(name, np_img)
+
+    data_dirs = []
+    for dirname in os.listdir(data_dir):
+        if dirname.startswith('LuoJia1'):
+            data_dirs.append(dirname)
+
+    data_dirs.sort()
+
+    print(data_dirs)
+
+    images = pmap(get_img, data_dirs)
+
+    pmap(wrapper, zip(data_dirs, images))
+
+    # for (name, im) in zip(data_dirs, images):
+    #     do_one(name, im)
+
+    img = get_img('LuoJia1-01_LR201809083317_20180907212040_HDR_0033')
